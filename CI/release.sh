d667824d29fbc9d3c453babbc4f804195073490a
Salvatore Previti <roorback@gmail.com>
2019-07-31 16:12:41 +0100

    release sh

--- CI/release.sh
+++ CI/release.sh
@@ -4,7 +4,8 @@ git fetch
 git checkout master
 git pull
 #npm i
-#npm run lint -- --max-warnings 0
-#npm test
+npm ci
+npm run lint -- --max-warnings 0
+npm test
 npm version patch -m "Version %s"
 git push --follow-tags
