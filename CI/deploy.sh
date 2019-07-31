09f0b5f3212dfbb3620b24065e29fd00f73c883d
Salvatore Previti <roorback@gmail.com>
2019-07-31 15:00:14 +0100

    deploy

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -1,6 +1,8 @@
 # exit when any command fails
 set -e
-npm version patch -m "Version %s"
-git push origin master
+npm version from-git -m "Version %s"
+npm ci
+npm run lint -- --max-warnings 0
+npm test
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
 npm publish --access public
