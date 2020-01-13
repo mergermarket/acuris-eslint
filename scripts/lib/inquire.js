c002ac259ce953e1b47be8f76a98a8d6c49abfe9
Salvatore Previti <roorback@gmail.com>
2020-01-13 17:37:08 +0000

    remove inquire dependency

--- scripts/lib/inquire.js
+++ scripts/lib/inquire.js
@@ -1,12 +1,20 @@
-const inquirer = require('inquirer')
-
-async function askConfirmation(message) {
-  const answer = await inquirer.prompt({
-    name: 'confirm',
-    type: 'confirm',
-    message
-  })
-  return !!answer.confirm
-}
+const chalk = require('chalk')
+const readline = require('readline')
 
 exports.askConfirmation = askConfirmation
+
+async function askConfirmation(message) {
+  if (!process.stdin || !process.stdout || !process.stdout.isTTY) {
+    return true
+  }
+  return new Promise(resolve => {
+    const iface = readline.createInterface(process.stdin, process.stdout)
+    const question = `${chalk.greenBright('?')} ${chalk.whiteBright(message)} ${chalk.gray('(Y/n)')} `
+    iface.question(question, answer => {
+      const confirm = /^[yY]/.test((answer || '').trim())
+      console.log(confirm ? chalk.greenBright('  Yes') : chalk.redBright('  No'))
+      iface.close()
+      resolve(confirm)
+    })
+  })
+}
